958d72c78701aed37eef459b1df05a1dd3fc6ccb
ElainOAO <lyn44203@gmail.com>
2018-08-01 14:13:12 +0800

    新增測試通知中心與推播設定的功能

---
+++ notice_test.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+#coding=utf-8
+import os
+import re
+import sys
+import pytest
+import logging
+import unittest
+import subprocess
+import time
+
+from appium import webdriver
+
+import config
+from RAT import *
+
+# Returns abs path relative to this file and not cwd
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
+
+path1 = "./test_resources/."
+
+# logger
+logger = logging.getLogger()
+logFormatter = logging.Formatter(
+    '[%(asctime)-15s][%(filename)s][%(funcName)s#%(lineno)d] %(message)s')
+ch = logging.StreamHandler(sys.stdout)
+ch.setFormatter(logFormatter)
+logger.addHandler(ch)
+logger.setLevel(logging.INFO)
+
+class NoticeTest(BaseTests):
+    def test_notification_center(self):
+        try:
+            #測試通知中心by有通知
+            self.sociushelper.click_login_by_email_link()
+            self.sociushelper.login_account("poi098@gmail.com", "poi098")
+            self.sociushelper.click_require_permission_button()
+            self.sociushelper.click_onboading_step()
+
+            self.sociushelper.login_point()
+            self.sociushelper.swipe_discover()
+
+            self.sociushelper.click_notification_button()
+            try:
+                self.assertTrue(self.sociushelper.click_notification_detail())
+                self.sociushelper.waitii()
+                self.sociushelper.go_back()
+                self.sociushelper.go_back()
+            except:
+                self.sociushelper.leave_button()
+        except :
+            self.logger.info('caught exception: {}'.format(sys.exc_info()[0]))
+            self.syshelper.capture_screen("test_notification_center")
+            raise
+
+    def test_blank_notification_center(self):
+        try:
+            #測試通知中心by無通知
+            self.sociushelper.click_login_by_email_link()
+            self.sociushelper.login_account("notice@test.com", "test123") 
+            self.sociushelper.click_require_permission_button()
+            self.sociushelper.click_onboading_step()
+
+            self.sociushelper.login_point()
+            self.sociushelper.swipe_discover()
+
+            self.sociushelper.click_notification_button()
+            try:
+                self.assertTrue(self.sociushelper.click_notification_detail())
+                self.sociushelper.go_back()
+                self.sociushelper.go_back()
+            except:
+                self.sociushelper.leave_button()
+        except :
+            self.logger.info('caught exception: {}'.format(sys.exc_info()[0]))
+            self.syshelper.capture_screen("test_blank_notification_center")
+            raise
+
+    def test_push_notification(self):
+        try:
+            #測試推播通知設定
+            self.sociushelper.click_login_by_email_link()
+            self.sociushelper.login_account("poi098@gmail.com", "poi098")
+            self.sociushelper.click_require_permission_button()
+            self.sociushelper.click_onboading_step()
+
+            self.sociushelper.login_point()
+            self.sociushelper.swipe_discover()
+            self.sociushelper.click_hamburger_button()
+            self.sociushelper.click_push_button()
+            self.sociushelper.stop_push_button()
+            self.sociushelper.start_push_button()
+            self.sociushelper.leave_button()
+        except :
+            self.logger.info('caught exception: {}'.format(sys.exc_info()[0]))
+            self.syshelper.capture_screen("test_push_notification")
+            raise
